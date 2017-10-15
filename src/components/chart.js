c670087dd0535ebbf01c5b1183be1aba224446cb
Chandler Stolworthy <chandler.stolworthy@gmail.com>
2017-10-14 21:46:17 -0600

    added project folders and files

---
+++ src/components/chart.js
@@ -0,0 +1,28 @@
+// 'use strict'
+
+// const data  = require('./chart-data')
+// const path = require('path')
+// const webpack = require('webpack')
+
+// module.exports = [{
+//     name: 'chartComponent',
+//     entry: {
+//       line: './line.jsx',
+//     },
+//     output: {
+//       path: data,
+//       filename: '[name].js'
+//     },
+//     module: {
+//       loaders: [
+//         {
+//           test: [/\.jsx$/],
+//           loaders: ["jsx-loader?insertPragma=React.DOM&harmony"],
+//         }
+//       ],
+//     },
+//     resolve: {
+//       extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx']
+//     }
+//   }];
+
