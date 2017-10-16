3a61593bd60655a8a76ce2da447bf53af8f88f59
Anthony <anthony.caccavella@gmail.com>
2017-10-16 16:54:42 -0600

    Basic outline of the initial landing page is done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom';
 import SignInPage from './components/sign-in-page/sign-in-page';
 // import HomePage from './';
 import Dashboard from './components/dashboard/dashboard';
-import CreateCompany from './components/create-company/create-company'
+// import CreateCompany from './components/create-company/create-company'
 
 class App extends Component {
   render() {
