9cce7e26201ed89c8600f804f0c5d148dfc3204d
alecjacksn <alecajackson@gmail.com>
2017-10-16 11:47:39 -0600

    fix

--- db/company/add_company.sql
+++ db/company/add_company.sql
@@ -1,10 +1,3 @@
 INSERT INTO company
 (company_name, company_phone, company_email, company_logo, company_date, company_industry)
-VALUES(
-    'Stance',
-    5552314321,
-    'email@gmail.com',
-    null,
-    '10/16/2017',
-    'Retail'
-)
+VALUES( $1, $2, $3, $4, $5, $6)
