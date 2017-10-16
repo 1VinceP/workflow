bb4065919c8135fe6747d4844edc1206ec3d9e90
Anthony <anthony.caccavella@gmail.com>
2017-10-16 11:55:16 -0600

    Added login setup with Auth0

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store'
+ReactDOM.render(
+    <HashRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </HashRouter>
+, document.getElementById('root'));
 registerServiceWorker();
