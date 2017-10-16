ae588a607fb0ce3f6f0938236f849d7e0754dca9
alecjacksn <alecajackson@gmail.com>
2017-10-16 11:41:07 -0600

    first commit. basic stucture

---
+++ db/company/add_company.sql
@@ -0,0 +1,10 @@
+INSERT INTO company
+(company_name, company_phone, company_email, company_logo, company_date, company_industry)
+VALUES(
+    'Stance',
+    5552314321,
+    'email@gmail.com',
+    null,
+    '10/16/2017',
+    'Retail'
+)
