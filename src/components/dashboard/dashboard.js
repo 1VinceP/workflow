71039e26fd6d168e6a9684d894c8a01c43a19580
Anthony <anthony.caccavella@gmail.com>
2017-10-16 15:33:50 -0600

    Created dashboard component

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import './dashboard.css';
+import FlatButton from 'material-ui/FlatButton';
 
 class Dashboard extends Component {
 
     render() {
-        return(
+        return (
             <div className="dashboard-view">
                 <div className="dash-wrapper">
-                    Test
+                    <div className="nav-placeholder">Nav</div>
+                    <div className="button-span">
+                        <FlatButton className="project-button"
+                            backgroundColor="#6acbe0"
+                            hoverColor="#6859ea"
+                            rippleColor="white"
+                            label="+ New Project" />
+                    </div>
+                    <div className="content-wrapper">
+                        <div className="left-side">
+                            <div className="notifications"></div>
+                            <div className="calendar"></div>
+                        </div>
+                        <div className="task-list"></div>
+                    </div>
+                    <div className="current-stats-wrapper"></div>
                 </div>
             </div>
         )
