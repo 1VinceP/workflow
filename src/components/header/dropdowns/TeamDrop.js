0aea6030a296a837f60a9168d7b6bf892a675f87
Vincent Palmer <vpalmergraphics@gmail.com>
2017-10-19 11:22:07 -0600

    Modified Header

---
+++ src/components/header/dropdowns/TeamDrop.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import {Link} from 'react-router-dom';
+import './dropdown.css'
+
+class TeamDrop extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            open: false
+        }
+
+        this.handleOpen = this.handleOpen.bind(this)
+        this.handleClose = this.handleClose.bind(this)
+    }
+
+    handleOpen( e ) {
+        e.preventDefault();
+
+        this.setState({
+            open: true,
+            anchorEl: e.currentTarget
+        })
+    }
+
+    handleClose() {
+        this.setState({
+            open: false
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <FlatButton onClick={this.handleOpen} label='Teams' />
+                <Popover open={this.state.open}
+                         onRequestClose={this.handleClose} 
+                         anchorEl={this.state.anchorEl}
+                         anchorOrigin={{horizontal: 'middle', vertical: 'bottom'}}
+                         targetOrigin={{horizontal: 'middle', vertical: 'top'}}
+                >
+                    <Menu>
+                        <Link to='/display-teams' className='header-link'><MenuItem primaryText='Teams' onClick={this.handleClose} /></Link>
+                        <Link to='/display-users' className='header-link'><MenuItem primaryText='Users' onClick={this.handleClose} /></Link>
+                    </Menu>
+                </Popover>
+            </div>
+        )
+    }
+}
+
+export default TeamDrop;
