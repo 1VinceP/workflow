ba065ae4c9f9efb910d71eb4065245552209daa5
Anthony <anthony.caccavella@gmail.com>
2017-10-24 11:35:36 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import Router from './router';
 class App extends Component {
   render() {
     return (
-
+      
       <div className="App">
+      <link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet"/>
         <Header />
         <div style={{width: '100%', height: '60px', background: '#F9F9FA'}} ></div>
         {Router}
